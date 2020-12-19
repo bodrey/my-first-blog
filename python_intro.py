4142c40a7cc2890d2ecb5a2804ece8ab89c87bd0
Andrey Martynov <andrey@martynov.info>
2020-12-19 17:13:55 +0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,9 @@
+# this is kinda comment
+
+names = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+def greet (name):
+    print('Hi, ' + str(name) + '!')
+
+for name in range(1, 6):
+    greet(name)
